45362c4b2ed83d2a64ca507e193df65b72539fff
Matt Oquist <moquist@majen.net>
2014-04-05 14:05:17 -0400

    note that a different structure would better

--- src/gangway/util.clj
+++ src/gangway/util.clj
@@ -1,6 +1,10 @@
 (ns gangway.util
   (:require [gangway.worker :as gw-worker]))
 
+;; TODO: Figure out how to support incoming and outgoing queues with
+;; the same key in a reasonable way.
+;; Before this question can be answered, must figure out how outgoing
+;; queues would be presented via REST.
 (def queues
   {:showevidence {:name "queue.showevidence-in"
                   :worker-fn gw-worker/do-work}})
