47dee625f5a850ab40ec7fb661bb1a6a8c6fd06b
Matt Oquist <moquist@majen.net>
2014-04-08 19:18:57 -0400

    clean up :require, add doc

--- src/gangway/web.clj
+++ src/gangway/web.clj
@@ -1,6 +1,9 @@
-(ns gangway.web
-  (:require [helmsman]
-            [ring.middleware.params :refer [wrap-params]]
+(ns ^{:doc
+      "Gangway is for queueing and (minimal, queue-wrapping) REST
+       endpoints for data entering or leaving Galleon. Nothing from
+       outside enters or leaves Galleon except by the Gangway."}
+  gangway.web
+  (:require [ring.middleware.params :refer [wrap-params]]
             [liberator.dev :refer [wrap-trace]]
             [gangway.publish :as gw-publish]))
 
