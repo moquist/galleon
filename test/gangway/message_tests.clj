e8f93f4174ca5cfaa6cc509b886991e5830aac8f
Tom Dolan <tgdolan@gmail.com>
2014-04-17 00:20:00 -0400

    Galleon : Test : Committing failing test.check defspec
    Committing a failing defspec to show basic test.check test and what
    the failing report looks like.
    
    This first test was to confirm that converting EDN to JSON and back
    resulted in identical data structures. Apparrently, that is not
    a valid assumption as the test fails (have not yet investigated).
    
    --Tom

--- test/gangway/message_tests.clj
+++ test/gangway/message_tests.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test.check :as tc]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop])) 
+            [clojure.test.check.properties :as prop]
+            [clojure.data.json :as json])) 
 
 ;; from test.check README.md
 (defspec first-element-is-min-after-sorting ;; the name of the test
@@ -10,3 +11,15 @@
          (prop/for-all [v (gen/such-that not-empty (gen/vector gen/int))]
            (= (apply min v)
               (first (sort v)))))
+
+;; VLACS data exchange messages are defined as a vector of maps.
+(defspec abstract-message
+  ;; testing that reading EDN data structure into JSON and converting
+  ;; back yields the same data structure.
+  ;; Note: This test fails. Committing failing test to show how
+  ;; test.check reports failures.
+  (prop/for-all [ab-message (gen/vector (gen/map gen/keyword gen/string))]
+    (= ab-message (-> ab-message
+                       json/json-str
+                       json/read-str))))
+
