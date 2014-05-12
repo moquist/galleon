344935489a7107e46389c2b20d51e6ebaa59ded2
Mike George <mikeyg6754@gmail.com>
2014-05-12 11:54:04 -0400

    remove voom versions from remote, add immutant as project plugin

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [^{:voom {:repo "https://github.com/vlacs/helmsman"}}
-                 [org.vlacs/helmsman "0.2.5-20140505_122511-g1af41e7"]
-                 [org.vlacs/traveler "0.2.10" :exclusions [org.vlacs/helmsman]]
-                 [org.immutant/immutant "1.1.1"
+                 [org.vlacs/helmsman "0.2.5"]
+                 [org.vlacs/traveler "0.2.10" :exclusions [org.vlacs/helmsman org.vlacs/hatch]]
+                 #_[org.immutant/immutant "1.1.1"
                   :exclusions [org.clojure/core.memoize io.netty/netty org.hornetq/hornetq-core-client]]
                  ^{:voom {:repo "https://github.com/vlacs/timber"}}
-                 [org.vlacs/timber "0.1.7-20140506_033559-gae34e51"]
+                 [org.vlacs/timber "0.1.7"]
                  #_[org.immutant/immutant-messaging "1.1.1"
                   :exclusions [io.netty/netty]]
                  ^{:voom {:repo "https://github.com/vlacs/navigator" :branch "master"}}
-                 [org.vlacs/navigator "0.1.0-20140506_040417-g8eca734"]
+                 [org.vlacs/navigator "0.1.0-SNAPSHOT"]
                  [http-kit "2.1.16"]
                  #_[ring "1.2.1"]
                  [liberator "0.11.0"]
@@ -30,15 +30,15 @@
   :profiles {:voom {:plugins [[lein-voom "0.1.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]}
              :dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [com.datomic/datomic-free "0.9.4707"
-                                   :exclusions [commons-codec]]
+                                   :exclusions [commons-codec org.jgroups/jgroups org.jboss.logging/jboss-logging]]
                                   [org.clojure/test.check "0.5.7"]]
                    :source-paths ["dev"]}
              :dev-pro {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                                       [com.datomic/datomic-free "0.9.4699"
-                                       :exclusions [commons-codec]]]
+                                       :exclusions [commons-codec org.jgroups/jgroups org.jboss.logging/jboss-logging]]]
                        :source-paths ["dev"]}
              :production {:dependencies [[com.datomic/datomic-free "0.9.4699"
-                                          :exclusions [commons-codec]]]}
+                                          :exclusions [commons-codec org.jgroups/jgroups org.jboss.logging/jboss-logging]]]}
              :production-pro {:repositories [["my.datomic.com" {:url "https://my.datomic.com/repo"
                                                                 :username :env/lein_datomic_repo_username
                                                                 :password :env/lein_datomic_repo_password}]]
