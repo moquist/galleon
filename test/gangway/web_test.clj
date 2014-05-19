443e00ed0531f766ccd15bfd658d8bcf17ba7089
Mike George <mikeyg6754@gmail.com>
2014-05-19 16:56:00 -0400

    update tests to check for valid and invalid message

--- test/gangway/web_test.clj
+++ test/gangway/web_test.clj
@@ -7,10 +7,37 @@
             [gangway.util :as gw-util]
             [gangway.web :as gw-web]))
 
-(deftest remote-http-test
+(def invalid-message
+  (json/json-str
+   {:header {:operation :assert
+             :entity-id {:task-id 137}
+             :entity-type :task}
+    :payload {:entity {:id-sk-origin "vlacs"
+                       :name "Master the art of addition"
+                       :description "This is some sort of description"}}}))
+
+(def valid-message
+  (json/json-str
+   {:header {:operation :assert
+             :entity-id {:task-id 17}
+             :entity-type :task}
+    :payload {:entity {:id-sk-origin "vlacs"
+                       :name "Master the art of addition"
+                       :version "v3"
+                       :description "This is some sort of description"}}}))
+
+(deftest remote-http-test-malformed
   (let [result (http/post
                 (format "%s/gangway/in/showevidence" (immutant.util/app-uri))
-                {:body (json/json-str {:a "b"})
+                {:body invalid-message
                  :throw-exceptions false})]
-    (testing "malformed handler"
+    (testing "malformed request (missing a required field)"
       (is (= 400 (:status result))))))
+
+(deftest remote-http-test-success
+  (let [result (http/post
+                (format "%s/gangway/in/showevidence" (immutant.util/app-uri))
+                {:body valid-message
+                 :throw-exceptions false})]
+    (testing "successful request"
+      (is (= 201 (:status result))))))
