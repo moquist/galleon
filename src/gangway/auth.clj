3fa8078fb752324b841c453ad37d8f95dc913f9b
Mike George <mikeyg6754@gmail.com>
2014-06-02 11:50:38 -0400

    pull token from datomic and make sure is is not expired

--- src/gangway/auth.clj
+++ src/gangway/auth.clj
@@ -1,13 +1,33 @@
 (ns gangway.auth
   (:require [clojure.string :refer [split]]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clj-time.core :as t]
+            [clj-time.coerce :as t-c]
+            [datomic.api :as d]
+            [gangway.schema :as schema]
+            [hatch]))
+
+(defn expired?
+  "Checks expiration time of token against current time
+
+  Params:  q-res - result of d/q
+           db    - Datomic database connection
+  Returns: boolean"
+  [q-res db]
+  (let [ent (d/entity (d/db db) (ffirst q-res))
+        expires (:queue-auth/expires ent)]
+    (if (nil? expires)
+      false
+      (if (t/after? (t/now) (t-c/from-date expires))
+        true
+        false))))
 
 (defn request->auth-token
   "Retrieves the authentication token from an incoming
-   request.
+  request.
 
-   Params:  ctx - Liberator Context
-   Returns: string"
+  Params:  ctx - Liberator Context
+  Returns: string"
   [ctx]
   (let [auth (get-in ctx [:request :headers "authorization"])]
     (if (nil? auth)
@@ -19,28 +39,49 @@
 
 (defn gen-token
   "Generates a random token of a specified length consisting
-   of numbers, letters, and symbols.
+  of numbers, letters, and symbols.
 
-   Params:  length - int
-   Returns: string"
+  Params:  length - int
+  Returns: string"
   [length]
   (apply str
-    (take length
-      (repeatedly
-        #(rand-nth "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_")))))
+         (take length
+               (repeatedly
+                #(rand-nth "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_")))))
+
+(defn add-queue-token!
+  "Add a token to use the queues, takes an owner as a string
+   and an exipration time expressed in the number of months from
+   the current time as an int"
+  [owner expire db]
+  (let [expiration (.toDate (t/plus (t/now) (t/months expire)))
+        token      (gen-token 64)
+        queue-auth {:token token
+                    :owner owner
+                    :expires expiration}]
+    (schema/tx-entity! db :queue-auth (hatch/slam-all queue-auth :queue-auth))
+    queue-auth))
+
 
 (defn validate-token
   "Validates a token from an incoming request,
-   designed to be used in the authorized? section
-   of a liberator resource.
+  designed to be used in the authorized? section
+  of a liberator resource.
 
-   TODO: Check if the incoming token exists in Datomic,
-         don't hard code this.
-
-   Params:  ctx - Liberator Context
-   Returns: boolean"
-  [ctx]
-  (let [token (request->auth-token ctx)]
-    (if (= token (str "T0_V&(1AZ#U1$X3EXQL@K!OJ568G4&3DL55!5MU16E#E6TY%KV!3O1QB&L2!QSXT"))
-      true
+  Params:  ctx - Liberator Context
+  db  - Datomic database connection
+  Returns: boolean"
+  [ctx db]
+  (let [token  (request->auth-token ctx)]
+    (if (not (empty? token))
+      (let [result (d/q '[:find ?e
+                          :in $ ?token
+                          :where [?e :queue-auth/token ?token]]
+                        (d/db db)
+                        token)]
+        (if (empty? result)
+          false
+          (if (expired? result db)
+            false
+            true)))
       false)))
