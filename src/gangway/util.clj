0676233c0065b7875c293cb404ccb81dc70aa59f
Matt Oquist <moquist@majen.net>
2014-06-04 15:46:53 -0400

    remove old comment

--- src/gangway/util.clj
+++ src/gangway/util.clj
@@ -3,27 +3,6 @@
             [gangway.worker]
             [gangway.disembark]))
 
-;; TODO: Figure out how to support incoming and outgoing queues with
-;; the same key in a reasonable way.
-;; Before this question can be answered, must figure out how outgoing
-;; queues would be presented via REST.
-
-#_
-(def queues-v2
-  [{:name "showevidence"
-    :in-fn blah
-    :out-fn blah}
-   {:name "genius"
-    :in-fn g-blah}])
-
-#_
-(def queues
-  {:showevidence {:name "queue.showevidence-in"
-                  ;; having a :worker-fn implies that galleon should start a listener
-                  :worker-fn gangway.worker/do-work}
-   :genius {:name "queue.genius-in"
-            :worker-fn gangway.worker/do-work}})
-
 (defn start-queue! [system {n :name worker :worker}]
   (msg/start n)
   (msg/listen n (partial worker system)))
@@ -37,7 +16,7 @@
 
 ;; TODO: handle exceptions
 (defn start-queues! [system]
-  (let [qids (get-in system [:flare :queues])
+  (let [qids (get-in system [:flare :attaches])
         queues (reduce (fn start-queues!- [c v]
                          (assoc c v (queue-definitions v)))
                        {} qids)
