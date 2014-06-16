df01ecc0f6bfef9e1c15c1275db858e2ff34e1d0
Matt Oquist <moquist@majen.net>
2014-06-16 16:23:11 -0400

    use voom best-practices and use -SNAPSHOT until cementing a release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.vlacs/galleon "0.1.0"
+(defproject org.vlacs/galleon "0.1.1-SNAPSHOT"
   :description "Galleon is a library that ties together multiple web app libraries
                to turn them into a single cohesive application."
   :url "https://www.github.com/vlacs/galleon"
